0b67d8ab6447c981c60d7695719092be0ffb4015
sagnikcapital <sagnikd@capitalnumbers.com>
2024-04-30 16:40:59 +0530

    angular-env-setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"learnfirestore-55715","appId":"1:733265813084:web:3ddbc7b44e9f04ab8f5099","storageBucket":"learnfirestore-55715.appspot.com","apiKey":"AIzaSyD6So4lkkVx90zu-YuJJ7kSs41nA6Ifj3A","authDomain":"learnfirestore-55715.firebaseapp.com","messagingSenderId":"733265813084","measurementId":"G-2SHS48JWGG"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp(environment.firebaseConfig)
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
