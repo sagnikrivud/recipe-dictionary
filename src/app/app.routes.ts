4565ad7e51a9d4c5748bed1f83879efc90d189b9
sagnikcapital <sagnikd@capitalnumbers.com>
2024-04-30 18:26:58 +0530

    anchor-tag

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { VegComponent } from './veg/veg.component';
 import { NonVegComponent } from './non-veg/non-veg.component';
 import { DessertComponent } from './dessert/dessert.component';
 import { JuiceComponent } from './juice/juice.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'veg', component: VegComponent },
   { path: 'non-veg', component: NonVegComponent },
   { path: 'dessert', component: DessertComponent },
