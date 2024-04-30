a7f9f226bf1588d7450a85ea39ccc910b4e87af2
sagnikcapital <sagnikd@capitalnumbers.com>
2024-04-30 16:47:17 +0530

    angular-env-setup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: process.env["apiKey"],
-    authDomain: process.env["authDomain"],
-    projectId: process.env["projectId"],
-    storageBucket: process.env["storageBucket"],
-    messagingSenderId: process.env["messagingSenderId"],
-    appId: process.env["appId"]
+    apiKey: '',//process.env["apiKey"],
+    authDomain: '',//process.env["authDomain"],
+    projectId: '',//process.env["projectId"],
+    storageBucket: '',//process.env["storageBucket"],
+    messagingSenderId: '',//process.env["messagingSenderId"],
+    appId: '',//process.env["appId"]
   }
 };
