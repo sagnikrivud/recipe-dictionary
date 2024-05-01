d77f3215ee021ebd25634ccd48ce71f2110e1e44
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-01 13:24:53 +0530

    jouice-page-implement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { NonVegComponent } from './non-veg/non-veg.component';
 import { DessertComponent } from './dessert/dessert.component';
 import { JuiceComponent } from './juice/juice.component';
 import { HomeComponent } from './home/home.component';
+import { VegItemComponent} from './veg/items/items.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'veg', component: VegComponent },
   { path: 'non-veg', component: NonVegComponent },
   { path: 'dessert', component: DessertComponent },
-  { path: 'juice', component: JuiceComponent }
+  { path: 'juice', component: JuiceComponent },
+  { path: 'veg/items', component:VegItemComponent}
 ];
