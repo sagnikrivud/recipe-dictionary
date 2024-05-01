4b362fa5a663c13d27ad180b35750b9476b46e84
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-01 15:18:28 +0530

    jouice-page-implement

--- src/app/veg/veg.component.ts
+++ src/app/veg/veg.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../layouts/header/header.component';
 import { FooterComponent } from '../layouts/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-veg',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './veg.component.html',
   styleUrl: './veg.component.css'
 })
