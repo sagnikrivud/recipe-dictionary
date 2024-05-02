6aa9b1d3574149c37d770c6b81480de56ee0e27c
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-02 13:18:35 +0530

    hover-card-zoom

--- dotenv.ts
+++ dotenv.ts
@@ -2,5 +2,10 @@ import { config } from 'dotenv';
 
 config();
 
-export const API_URL = process.env.API_URL;
-export const APIKEY = process.env.apiKey;
+export const API_URL  = process.env['API_URL'];
+export const API_KEY  = process.env['apiKey'];
+export const AUTH_DOMAIN = process.env['authDomain'];
+export const PROJECT_ID = process.env['projectId'];
+export const STORAGE_BUCKET = process.env['storageBucket'];
+export const MESSAGE_ID = process.env['messagingSenderId'];
+export const APP_ID = process.env['appId'];
