b6d92cae6664e20ef03e72da355269dca4e7beda
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-03 17:44:49 +0530

    get-firestore-data

---
+++ src/app/itemdetails/itemdetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itemdetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './itemdetails.component.html',
+  styleUrl: './itemdetails.component.css'
+})
+export class ItemdetailsComponent {
+
+}
